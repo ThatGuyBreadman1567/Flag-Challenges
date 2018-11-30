25aacc496bfd3eeec533564e175e1c5e35a29977
breadman22 <42971506+ThatGuyBreadman1567@users.noreply.github.com>
2018-11-30 10:21:52 -0500

    Add files via upload

---
+++ FlagChallenge7.py
@@ -0,0 +1,62 @@
+# Switzerland
+# Ben WON
+
+import turtle
+
+x = 1
+t = turtle
+l = t.left
+r = t.right
+f = t.forward
+b = t.begin_fill
+e = t.end_fill
+c = t.fillcolor
+u = t.penup
+d = t.pendown
+p = t.pencolor
+s = t.speed
+
+c('red')
+b()
+f(320)
+l(90)
+f(320)
+l(90)
+f(320)
+l(90)
+f(320)
+e()
+l(90)
+f(130)
+l(90)
+u()
+f(60)
+d()
+c('white')
+b()
+f(70)
+l(90)
+f(70)
+r(90)
+f(60)
+r(90)
+f(70)
+l(90)
+f(70)
+r(90)
+f(60)
+r(90)
+f(70)
+l(90)
+f(70)
+r(90)
+f(60)
+r(90)
+f(70)
+l(90)
+f(70)
+r(90)
+f(60)
+e()
+
+turtle.done()
