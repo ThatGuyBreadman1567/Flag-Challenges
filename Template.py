6ff5dced526e215f99e1e1f4e3d7b3977ddf6355
breadman22 <42971506+ThatGuyBreadman1567@users.noreply.github.com>
2018-11-30 10:02:38 -0500

    Add files via upload

---
+++ Template.py
@@ -0,0 +1,18 @@
+import turtle
+
+x = 1
+t = turtle
+l = t.left
+r = t.right
+f = t.forward
+b = t.begin_fill
+e = t.end_fill
+c = t.fillcolor
+u = t.penup
+d = t.pendown
+p = t.pencolor
+s = t.speed
+
+
+
+turtle.done()
