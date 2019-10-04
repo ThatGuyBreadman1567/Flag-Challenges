0a5d81df031c8e02e370cce8c207b4dcfeadd9ac
breadman22 <42971506+ThatGuyBreadman1567@users.noreply.github.com>
2019-10-04 10:22:25 -0400

    Update Template.py

--- Template.py
+++ Template.py
@@ -1,17 +1,21 @@
 import turtle
+from turtle import *
+
+#
+#
 
 x = 1
-t = turtle
-l = t.left
-r = t.right
-f = t.forward
-b = t.begin_fill
-e = t.end_fill
-c = t.fillcolor
-u = t.penup
-d = t.pendown
-p = t.pencolor
-s = t.speed
+hideturtle()
+l = left
+r = right
+f = forward
+b = begin_fill
+e = end_fill
+c = fillcolor
+u = penup
+d = pendown
+p = pencolor
+s = speed
 
 
 
